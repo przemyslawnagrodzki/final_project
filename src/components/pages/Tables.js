b81067084a03abf11883a00ba39c09130d0e5b80
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-04-18 14:50:32 +0200

    ADD tables

--- src/components/pages/Tables.js
+++ src/components/pages/Tables.js
@@ -1,8 +1,113 @@
+import { useState } from "react";
+import { Form, Row, Col, Stack, Button } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTableById } from "../../redux/tablesRedux";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { updateTable } from "../../redux/tablesRedux"
 
-const Tables  = () => {
-    return(
-        <h1>Tables</h1>
+
+const Tables = () => {
+  const { id } = useParams();
+  const table = useSelector(state => getTableById(state, id));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const options = ["Free", "Busy", "Reserved", "Cleaning"];
+  const [status, setStatus] = useState(table?.status);
+  const [bill, setBill] = useState(table?.bill);
+  const [people, setPeople] = useState(table?.peopleAmount);
+  const [maxPeople, setMaxPeople] = useState(table?.maxPeopleAmount);
+
+  useEffect(() => {
+    if (table) {
+      setStatus(table.status)
+      setBill(table.bill);
+      setPeople(table.peopleAmount);
+      setMaxPeople(table.maxPeopleAmount);
+    }
+  }, [table]);
+
+useEffect(() =>{
+  if(status === 'Busy') {
+    setBill(0);
+  } else if(status === 'Cleaning' || status === 'Free'){
+    setPeople(0);
+  }
+  if(people > maxPeople) {
+    setPeople(setMaxPeople);
+  }
+}, [status, people, maxPeople]);
+
+
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(updateTable(id, { status, bill, peopleAmount: people, maxPeopleAmount: maxPeople }));
+    navigate("/");
+  };
+
+
+
+
+  if (!table)
+    return (<h2>Loading...</h2>)
+
+  return(
+        <div>
+          <h1 className="my-2">Table {id}</h1>
+          <Form onSubmit={submit}>
+            <Form.Group as={Row} className="my-3">
+              <Form.Label column sm={2} lg={1}><strong>Status:</strong></Form.Label>
+              <Col sm={6} lg={3}>
+                <Form.Select value={status} onChange={(e) => setStatus(e.target.value)} className="form-select" aria-label="Default select example">
+                  {options.map((value) => (<option value={value} key={value}>{value}</option>))}
+                </Form.Select>
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="my-3">
+              <Form.Label column sm={1} ><strong>People:</strong></Form.Label>
+              <Col sm={1} >
+                <Form.Control
+                  type="number"
+                  value={people}
+                  onChange={(e) => setPeople(e.target.value)}
+                  min="0"
+                  max={maxPeople}
+                />
+              </Col>
+              /
+              <Col sm={1} >
+                <Form.Control
+                  type="number"
+                  value={maxPeople}
+                  onChange={(e) => setMaxPeople(e.target.value)}
+                  min="0"
+                  max="10"
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="my-3">
+              <Stack direction="horizontal">
+                <Form.Label column sm={1} lg={1} ><strong>Bill:</strong></Form.Label>
+                <p className="m-2">$</p>
+                <Col sm={1} >
+                  <Form.Control className="my-2"
+                    type="number"
+                    value={bill}
+                    onChange={(e) => setBill(e.target.value)}
+                    min="0"
+                    max="1000"
+                  />
+                </Col>
+              </Stack>
+            </Form.Group >
+              <Button  variant="primary" type="submit">Update</Button>
+          </Form>
+        </div>
+
     )
-}
-
+};
 export default Tables
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,14 +1,19 @@
 import axios from 'axios'
 
+
 //selectors
 export const getAllTables = ({ tables }) => tables
+export const getTableById = ({ tables }, tableId) => tables.find(tables => tables.id === tableId);
 
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
-
+const UPDATE_TABLES = createActionName('UPDATE_TABLES');
+const UPDATE_TABLE = createActionName('UPDATE_TABLE');
 export const FETCH_TABLES_SUCCESS = createActionName('FETCH_TABLES_SUCCESS');
 
 // action creators
+export const updateTables = payload => ({ type: UPDATE_TABLES, payload })
+export const updateTable = payload => ({ type: UPDATE_TABLE, payload })
 export const fetchTablesSuccess = tables => ({
   type: FETCH_TABLES_SUCCESS,
   payload: tables,
@@ -24,19 +29,22 @@ export const fetchTables = () => dispatch => {
     });
 };
 
-const initialState = {
-  tables: [],
-};
 
-const tablesReducer = (state = initialState, action) => {
+
+const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
     case FETCH_TABLES_SUCCESS:
       return {
-        ...state,
+        ...statePart,
         tables: action.payload,
       };
+    case UPDATE_TABLES:
+      return [...action.payload]
+
+    case UPDATE_TABLE:
+      return statePart.map(table => table.id === action.payload.id ? action.payload : table);
     default:
-      return state;
+      return statePart;
   };
 };
 
